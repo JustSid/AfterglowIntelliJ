61ee47781e41d5e19ee0f51ca1b5128a445ceda2
Sidney Just <sidney.just@gmail.com>
2015-11-27 09:18:44 +0000

    Much improved file type detection

--- src/com/widerwille/afterglow/AfterglowIcons.java
+++ src/com/widerwille/afterglow/AfterglowIcons.java
@@ -1,14 +1,12 @@
 package com.widerwille.afterglow;
 
-import com.intellij.lang.Language;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
-import com.intellij.util.PlatformUtils;
-import com.sun.istack.internal.Nullable;
+
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
@@ -18,17 +16,23 @@ public class AfterglowIcons
 	public static final Icon ANY = IconLoader.getIcon("/icons/file_type_default.png");
 	public static final Icon CFILE = IconLoader.getIcon("/icons/file_type_c.png");
 	public static final Icon CPPFILE = IconLoader.getIcon("/icons/file_type_c++.png");
+	public static final Icon CSHARP = IconLoader.getIcon("/icons/file_type_c#.png");
 	public static final Icon MFILE = IconLoader.getIcon("/icons/file_type_objectivec.png");
 	public static final Icon HEADER = IconLoader.getIcon("/icons/file_type_header.png");
+	public static final Icon HASKELL = IconLoader.getIcon("/icons/file_type_haskell.png");
 	public static final Icon RUBY = IconLoader.getIcon("/icons/file_type_ruby.png");
 	public static final Icon PYTHON = IconLoader.getIcon("/icons/file_type_python.png");
 	public static final Icon JAVASCRIPT = IconLoader.getIcon("/icons/file_type_js.png");
+	public static final Icon COFFEE = IconLoader.getIcon("/icons/file_type_coffescript.png");
+	public static final Icon JAVA = IconLoader.getIcon("/icons/file_type_java.png");
+	public static final Icon PHP = IconLoader.getIcon("/icons/file_type_php.png");
 	public static final Icon SHELL = IconLoader.getIcon("/icons/file_type_source.png");
 	public static final Icon MARKDOWN = IconLoader.getIcon("/icons/file_type_markdown.png");
 	public static final Icon GIT = IconLoader.getIcon("/icons/file_type_git.png");
 	public static final Icon FONT = IconLoader.getIcon("/icons/file_type_font.png");
 	public static final Icon HTML = IconLoader.getIcon("/icons/file_type_html.png");
 	public static final Icon CSS = IconLoader.getIcon("/icons/file_type_css.png");
+	public static final Icon JADE = IconLoader.getIcon("/icons/file_type_jade.png");
 	public static final Icon XML = IconLoader.getIcon("/icons/file_type_markup.png");
 	public static final Icon JSON = IconLoader.getIcon("/icons/file_type_settings.png");
 	public static final Icon YAML = IconLoader.getIcon("/icons/file_type_yaml.png");
@@ -37,9 +41,44 @@ public class AfterglowIcons
 	public static final Icon BINARY = IconLoader.getIcon("/icons/file_type_binary.png");
 	public static final Icon TEXT = IconLoader.getIcon("/icons/file_type_text.png");
 	public static final Icon IMAGE = IconLoader.getIcon("/icons/file_type_image.png");
+	public static final Icon SQL = IconLoader.getIcon("/icons/file_type_sql.png");
 
 	public AfterglowIcons()
 	{}
+
+	@Nullable
+	public static final Icon getIconForExtension(@NotNull String extension)
+	{
+		switch(extension)
+		{
+			case "md":
+			case "markdown":
+				return MARKDOWN;
+
+			case "gitignore":
+			case "gitmodules":
+				return GIT;
+
+			case "npmignore":
+				return NPM;
+
+			case "sh":
+				return SHELL;
+
+			case "py":
+				return PYTHON;
+			case "rb":
+				return RUBY;
+			case "js":
+				return JAVASCRIPT;
+			case "ttf":
+			case "otf":
+			case "ttc":
+				return FONT;
+		}
+
+		return null;
+	}
 
 	@Nullable
 	public static final Icon getIcon(VirtualFile file, int flags, @Nullable Project project)
@@ -47,143 +86,109 @@ public class AfterglowIcons
 		if(file.isDirectory())
 			return DIRECTORY;
 
+		FileType type = file.getFileType();
+		String typeString = type.getName().toLowerCase();
 		String extension = file.getExtension();
-		if(extension == null)
+
+		if(extension != null)
+			extension = extension.toLowerCase();
+		else
+			extension = "";
+
+		switch(typeString)
 		{
-			switch(file.getName())
+			case "objectivec":
 			{
-				case "Podfile":
-					return RUBY;
-			}
+				switch(extension)
+				{
+					case "h":
+					case "hpp":
+					case "pch":
+						return HEADER;
+					case "cpp":
+						return CPPFILE;
+					case "c":
+						return CFILE;
+					case "mm":
+					case "m":
+						return MFILE;
+				}
 
-			return ANY;
-		}
-
-		switch(extension.toLowerCase())
-		{
-			case "h":
-			case "hpp":
-			case "pch":
-				return HEADER;
-			case "cpp":
-				return CPPFILE;
-			case "c":
 				return CFILE;
-			case "mm":
-			case "m":
-				return MFILE;
+			}
+			case "c#":
+				return CSHARP;
+			case "c++":
+				return CPPFILE;
 
-			case "gitignore":
-			case "gitmodules":
-				return GIT;
-
-			case "dylib":
-			case "tbd":
-			case "storyboard":
-			case "xib":
-			case "xcdatamodel":
-			case "xcdatamodeld":
-				return BINARY;
-
-			case "xcodeproj":
-				return null;
-
-			case "py":
-				return PYTHON;
-			case "rb":
-				return RUBY;
-			case "js":
-				switch(file.getName())
-				{
-					case "Gruntfile.js":
-						return GRUNT;
-				}
-
-				return JAVASCRIPT;
-
-			case "md":
-			case "markdown":
-				return MARKDOWN;
-
-			case "sh":
-				return SHELL;
-
-			case "txt":
-			case "strings":
-				// Special case
-				if(PlatformUtils.isCLion())
-				{
-					switch(file.getName())
-					{
-						case "CMakeLists.txt":
-							return null;
-					}
-				}
-
-				return TEXT;
-
-			case "cmake":
-				return (PlatformUtils.isCLion()) ? null : TEXT;
-
-			case "ttf":
-			case "otf":
-			case "ttc":
-				return FONT;
-
-			// Markup
 			case "json":
-				switch(file.getName())
-				{
-					case "package.json":
-						return NPM;
-				}
+				if(file.getName().equalsIgnoreCase("package.json"))
+					return NPM;
 
 				return JSON;
 			case "xml":
 			case "plist":
 				return XML;
-			case "yml":
-				return YAML;
+
 			case "html":
 			case "xhtml":
 				return HTML;
 			case "css":
 				return CSS;
+			case "yaml":
+				return YAML;
+			case "jade":
+				return JADE;
 
+			case "javascript":
+			case "ecmascript 6":
 
-			case "png":
-			case "jpg":
-			case "tga":
-			case "bmp":
-			case "gif":
+				if(file.getName().equalsIgnoreCase("gruntfile.js"))
+					return GRUNT;
+
+				return JAVASCRIPT;
+			case "coffeescript":
+			case "literal coffeescript":
+				return COFFEE;
+			case "ruby":
+			case "podfile":
+				return RUBY;
+			case "python":
+				return PYTHON;
+			case "php":
+				return PHP;
+			case "haskell":
+				return HASKELL;
+			case "java":
+				return JAVA;
+
+			case "images":
 				return IMAGE;
+			case "sql":
+				return SQL;
+
+			case "cmakelists.txt":
+				return null; // Use the default CMake icon
+
+			case "strings":
+				return TEXT;
+
+
+			case "plain_text":
+			{
+				Icon icon = getIconForExtension(extension);
+				if(icon != null)
+					return icon;
+
+				return TEXT;
+			}
+
+			case "unknown":
+				Icon icon = getIconForExtension(extension);
+				if(icon != null)
+					return icon;
 
 			default:
-				Language language = null;
-
-				if(project != null)
-				{
-					PsiManager psiManager = PsiManager.getInstance(project);
-					PsiFile psiFile = psiManager.findFile(file);
-
-					if(psiFile != null)
-						language = psiFile.getLanguage();
-				}
-
-				if(language != null)
-				{
-					switch(language.getID())
-					{
-						case "ObjectiveC":
-							return CFILE;
-						case "XML":
-							return XML;
-						case "JSON":
-							return JSON;
-					}
-				}
-
-				FileType type = file.getFileType();
 				return type.isBinary() ? BINARY : ANY;
 		}
 	}
